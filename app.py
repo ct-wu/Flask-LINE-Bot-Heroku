5bdbf3f8d2cad0ac447c14a927dce6ef52616871
CT Wu <ct@shoplineapp.com>
2023-08-31 10:45:42 +0800

    feat: connect to mongo

--- app.py
+++ app.py
@@ -8,10 +8,13 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
+from pymongo.mongo_client import MongoClient
+
 app = Flask(__name__)
 
 line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
 handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
+mongo_client = MongoClient(os.environ.get("MONGODB_URI"))
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -31,11 +34,43 @@ def callback():
         return "OK"
 
 
+usage = '''使用說明
+<項目><空格><金額>
+例如：早餐 100
+也支援透過半形逗號分隔多筆
+例如：早餐 100, 午餐 200
+'''
+
+def proc_msg(uid, msg):
+    if msg.startswith("/"):
+        return usage
+    elif msg.count(' ') > 0:
+        tokens = [x.strip() for x in msg.split(',')]
+        data = []
+        for token in tokens:
+            try:
+                item, cost = [t(s) for t,s in zip((str,int), token.split())]
+            except Exception as e:
+                return e
+
+            data.push({'item': item, 'cost': cost})
+
+        try:
+            client['accounting'].uid.insert_many(data)
+            return f'成功寫入{len(data)}筆'
+        except Exception as e:
+            return e
+
+    return usage
+        
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     get_message = event.message.text
     user_id = event.source.user_id
 
+    ret = proc_msg(user_id, get_message)
+
     # Send To Line
-    reply = TextSendMessage(text=f"{user_id}: {get_message}")
+    reply = TextSendMessage(text=f"{user_id}: {ret}")
     line_bot_api.reply_message(event.reply_token, reply)
